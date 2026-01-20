9d730ad43ecf08baefbf484efde6ae18623dda79
정성국 <ssoup0224@gmail.com>
2026-01-20 22:08:58 +0900

    edited eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
+  js.configs.recommended,
+  ...nextVitals,
+  ...nextTs,
 ]);
 
 export default eslintConfig;
